fec3f60712b7ef74c3c06d73f0253f27819d99db
ark8dev <aravindreddykarne@gmail.com>
2025-11-11 10:27:21 +0530

    Update keypad_defines.h

--- keypad_defines.h
+++ keypad_defines.h
@@ -1,3 +1,4 @@
+// keypad defines
 #define ROW0 16 // pin 1.16
 #define ROW1 17
 #define ROW2 18
@@ -8,14 +9,3 @@
 #define COL2 22
 #define COL3 23
 
-#define WRITEBYTE(word,bytestartpos,val)\
-                (word=((word&~(0xff<<bytestartpos))|(val<<bytestartpos)))
-#define READBIT(word,bitpos)\
-                ((word>>bitpos)&1)
-#define WRITEBIT(word,bitpos,val)\
-                (word=((word&~(1<<bitpos))|(val<<bitpos)))
-#define WRITENIBBLE(word,nibblestartpos,val)\
-                   (word=((word&~(0xf<<nibblestartpos))|(val<<nibblestartpos)))
-
-#define READNIBBLE(word,nibblestartpos)\
-                 ((word>>nibblestartpos)&0xf)
