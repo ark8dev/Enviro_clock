0f886a3d85619ecf38e96c8f2e46d5192cc1ee5e
ark8dev <aravindreddykarne@gmail.com>
2025-11-30 18:48:05 +0530

    Update headers.h

--- include/headers.h
+++ include/headers.h
@@ -1,1 +1,19 @@
-
+#ifndef __HEADERS_H__
+#define __HEADERS_H__
+#include<lpc214x.h>
+#include "defines.h"
+#include "types.h"
+#include "delay.h"
+#include "lcd.h"
+#include "lcd_defines.h"
+#include "kpm.h"
+#include "kpm_defines.h"
+#include "rtc.h"
+#include "rtc_defines.h"
+#include "enviro_clock.h"
+#include "adc.h"
+#include "adc_defines.h"
+#include "ext_int.h"
+#include "pin_connect_block.h"
+#include "pin_function_defines.h"
+#endif
